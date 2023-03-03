4d3e8b79f13fbe8755d285062a89b84b31ac9d52
Reagan615 <419247451@qq.com>
2023-03-03 12:11:18 -0600

    添加项目文件。

---
+++ Advanced-Database-and-ORM-Concepts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Advanced_Database_and_ORM_Concepts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
