92a9227ab8097aac644657a27cf0939822acba28
Reagan615 <419247451@qq.com>
2023-03-16 21:22:43 -0500

    commit

---
+++ Advanced-Database-and-ORM-Concepts_Lab03/Models/Room.cs
@@ -0,0 +1,14 @@
+﻿namespace Advanced_Database_and_ORM_Concepts_Lab03.Models
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public int Capacity { get; set; }
+        public enum Section
+        {
+            First,
+            Second,
+            Third
+        }
+    }
+}
